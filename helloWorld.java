74b73505c7bb2e5e90d17adac4c43a0d988d96be
Ilham Dwi Nugraha <ilhamdn99@gmail.com>
2019-12-05 13:17:54 +0700

    membuat contoh class dan objek segitiga

--- helloWorld.java
+++ helloWorld.java
@@ -2,6 +2,7 @@ public class helloWorld{
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        
         mobil bmw = new mobil();
         bmw.merk = "BMW-5100";
         bmw.maju();
@@ -9,6 +10,10 @@ public class helloWorld{
         mobil toyota = new mobil();
         toyota.merk = "Toyota Avanza";
         toyota.maju();
+
+        Segitiga segitiga = new Segitiga();
+
+        segitiga.hitung(10,5);
     }
 }
 
@@ -17,5 +22,14 @@ class mobil{
 
     public void maju(){
         System.out.println("Mobil "+this.merk+" maju");
+    }
+}
+
+class Segitiga{
+
+    public void hitung(int Alas, int Tinggi){
+        int luas;
+        luas = (Alas*Tinggi)/2;
+        System.out.println("Hasilnya : "+luas);
     }
 }
