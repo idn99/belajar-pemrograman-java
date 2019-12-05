94dadcb4b3381d011d52a3b79273b02f0b623b76
Ilham Dwi Nugraha <ilhamdn99@gmail.com>
2019-12-05 12:35:49 +0700

    membuat contoh class dan objek

--- helloWorld.java
+++ helloWorld.java
@@ -2,5 +2,20 @@ public class helloWorld{
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        mobil bmw = new mobil();
+        bmw.merk = "BMW-5100";
+        bmw.maju();
+
+        mobil toyota = new mobil();
+        toyota.merk = "Toyota Avanza";
+        toyota.maju();
+    }
+}
+
+class mobil{
+    public String merk;
+
+    public void maju(){
+        System.out.println("Mobil "+this.merk+" maju");
     }
 }
